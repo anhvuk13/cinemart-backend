53077061d0ab763e20d4f1437c0cd8ebb46877a5
anhvuk13 <anvut20@gmail.com>
2020-10-28 21:14:10 +0700

    init

---
+++ src/cinemart/core.clj
@@ -0,0 +1,66 @@
+(ns cinemart.core
+  (:require [org.httpkit.server :refer [run-server]]
+            [reitit.ring :as ring]
+            [ring.middleware.cors :refer [wrap-cors]]
+            [reitit.ring.middleware.exception :refer [exception-middleware]]
+            [reitit.ring.middleware.parameters :refer [parameters-middleware]]
+            [reitit.ring.middleware.muuntaja :refer [format-negotiate-middleware
+                                                     format-request-middleware
+                                                     format-response-middleware]]
+            [reitit.ring.coercion :refer [coerce-exceptions-middleware
+                                          coerce-request-middleware
+                                          coerce-response-middleware]]
+            [reitit.coercion.schema]
+            [muuntaja.core :as m]
+            [cinemart.routes :refer [ping-routes contact-routes]]))
+
+(defonce server (atom nil))
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    [["/api"
+      ping-routes
+      contact-routes]]
+    {:data {:coercion reitit.coercion.schema/coercion
+            :muuntaja m/instance
+            :middleware [[wrap-cors
+                          :access-control-allow-origin [#"http://localhost:4200"]
+                          :access-control-allow-methods [:get :post :put :delete]]
+                         parameters-middleware
+                         format-negotiate-middleware
+                         format-response-middleware
+                         exception-middleware
+                         format-request-middleware
+                         coerce-exceptions-middleware
+                         coerce-request-middleware
+                         coerce-response-middleware]}})
+   (ring/routes
+    (ring/redirect-trailing-slash-handler)
+    (ring/create-default-handler
+     {:not-found (constantly {:status 404 :body "Route not found"})}))))
+
+(defn stop-server []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)))
+
+(defn -main []
+  (println "Server started")
+  (reset! server (run-server app {:port 4000})))
+
+(defn restart-server []
+  (stop-server)
+  (-main))
+
+(comment
+  (app {:request-method :get
+        :uri "/api/invalid"})
+  (app {:request-method :get
+        :uri "/api/contacts"})
+  (app {:request-method :delete
+        :uri "/api/contacts/1"})
+  (app {:request-method :post
+        :uri "/api/contacts/"
+        :body "{\"first-name\":\"Kelvin\",\"last-name\":\"Mai\",\"email\":\"kelvin.mai002@gmail.com\"}"})
+  (restart-server))
---
+++ src/cinemart/db.clj
@@ -0,0 +1,12 @@
+(ns cinemart.db
+  (:require [hugsql.core :as hugsql]))
+
+(def config
+  {:classname "org.postgresql.Driver"
+   :subprotocol "postgresql"
+   :subname "//localhost:5432/clj_contacts"
+   :user "postgres"
+   :password "postgres"})
+
+(hugsql/def-db-fns "sql/contacts.sql")
+
