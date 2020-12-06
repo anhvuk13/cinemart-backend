7c126126fe8f60b1246da365286fe4602bb16866
anhvuk13 <anvut20@gmail.com>
2020-12-06 22:40:24 +0700

    fixed managers.sql theaters misspelling

--- resources/sql/managers.sql
+++ resources/sql/managers.sql
@@ -17,10 +17,12 @@ DROP TABLE IF EXISTS managers;
 SELECT * FROM managers;
 
 -- :name get-managers-without-pass :? :*
-SELECT mr.id, mail, mr.created_at, name, address
+SELECT mr.id, mr.mail, mr.created_at,
+t.id as theater_id, t.name as theater_name,
+t.address as theater_address
 FROM managers as mr
 inner join management as mt on mr.id = mt.manager
-inner join theater as t on mt.theater = t.id;
+inner join theaters as t on mt.theater = t.id;
 
 -- :name get-manager-by-id :? :1
 SELECT * FROM managers
